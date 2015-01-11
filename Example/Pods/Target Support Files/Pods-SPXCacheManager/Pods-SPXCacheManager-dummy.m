a3b8edf5f804b86ec472394b85aa356a7ab4abea
Shaps Mohsenin <shaps@theappbusiness.com>
2015-01-11 15:27:07 +0000

    Added initial files

---
+++ Example/Pods/Target Support Files/Pods-SPXCacheManager/Pods-SPXCacheManager-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SPXCacheManager : NSObject
+@end
+@implementation PodsDummy_Pods_SPXCacheManager
+@end
---
+++ Example/Pods/Target Support Files/Pods-SPXCacheManager/Pods-SPXCacheManager-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// SPXCacheManager
+#define COCOAPODS_POD_AVAILABLE_SPXCacheManager
+#define COCOAPODS_VERSION_MAJOR_SPXCacheManager 0
+#define COCOAPODS_VERSION_MINOR_SPXCacheManager 1
+#define COCOAPODS_VERSION_PATCH_SPXCacheManager 0
+
